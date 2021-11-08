065b3222cab19ff10a278602ea982b353335de69
brianorora <brianorora@gmail.com>
2021-11-08 22:37:28 +0300

    Remove underline from logo link

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 const Header = () => (
   <header className={styles.header}>
     <div className={styles.logo}>
       <img id="logo" src="https://cdn-icons-png.flaticon.com/512/3212/3212608.png" alt="" />
-      <h1><NavLink to="/" activeClassName={styles.active}>Space Travelers</NavLink></h1>
+      <h1><Link exact to="/">Space Travelers</Link></h1>
     </div>
     <ul>
       <li><NavLink to="/rockets" activeClassName={styles.active}>Rockets</NavLink></li>
