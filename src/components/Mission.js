24830dd6bc9aa293858cb7f4e563a64595ac7fc1
brianorora <brianorora@gmail.com>
2021-11-08 22:59:16 +0300

    Add `reserved` state conditional rendering

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -5,24 +5,30 @@ const Mission = ({
   name,
   /* eslint-disable-next-line */
   id,
+  reserved,
   description,
 }) => (
   <>
     <h2>{name}</h2>
     <p>{description}</p>
     <div className={styles.status}>
-      <span className="badge">Active Member</span>
+      <span className={reserved && styles.reserved}>{reserved ? 'Active Member' : 'NOT MEMBER'}</span>
     </div>
     <div className={styles.status}>
-      <button type="button">Join Mission</button>
+      <button type="button" className={reserved && styles.reserved}>{reserved ? 'Join Mission' : 'Leave Mission'}</button>
     </div>
   </>
 );
+
+Mission.defaultProps = {
+  reserved: false,
+};
 
 Mission.propTypes = {
   name: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  reserved: PropTypes.bool,
 };
 
 export default Mission;
