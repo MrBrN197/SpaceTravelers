706946b2c1632c1f0ce427056c2d05a0b6cde4ca
brianorora <brianorora@gmail.com>
2021-11-08 16:33:36 +0300

    Add `fetchMissions` action thunk

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -6,11 +6,20 @@ const LEAVE_MISSION = 'spaceTravelers/missions/LEAVE_MISSION';
 // Reducer
 export default (state = [], action = {}) => {
   switch (action.type) {
-    // do reducer stuff
+    case LOAD_MISSIONS:
+      return action.payload;
     default: return state;
   }
 };
 // Action Creators
-export const loadMissions = () => ({ type: LOAD_MISSIONS });
+export const loadMissions = (missions) => ({ type: LOAD_MISSIONS, payload: missions });
 export const joinMission = () => ({ type: JOIN_MISSION });
 export const cancelMission = () => ({ type: LEAVE_MISSION });
+
+// side effects, only as applicable
+// e.g. thunks, epics, etc
+export const fetchMissions = () => async (dispatch) => {
+  const data = [];
+  dispatch(loadMissions(data));
+  return data;
+};
