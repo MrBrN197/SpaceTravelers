7b3ba598100752797c42b320302ebde8fcaa7695
brianorora <brianorora@gmail.com>
2021-11-08 16:20:05 +0300

    Add `<Missions />` component to `App.js`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import Header from './Header';
+import Missions from './components/Missions';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Header />
       <Switch>
         <Route path="/" exact>this is hompeage</Route>
-        <Route path="/missions" exact>these are missions</Route>
+        <Route path="/missions" exact><Missions /></Route>
         <Route path="/rockets" exact>these are rockets</Route>
         <Route path="/profile" exact>this is the profile</Route>
       </Switch>
