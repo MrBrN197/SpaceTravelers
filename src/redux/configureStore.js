87c4510373b490978ed22a78c5efd3ee322e96f4
Bishoy <bishoy.samwuel@gmail.com>
2021-11-09 10:24:18 +0200

    Add the rocketsReducer to the configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { rocketsReducer } from './rockets/rockets';
 
 const reducer = combineReducers({
   // additional reducers could be added here
+  rockets: rocketsReducer,
 });
 
 const store = createStore(
