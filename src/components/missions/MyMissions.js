0d0860e1a00dde00cef7d924705dea45e93b5dc4
brianorora <brianorora@gmail.com>
2021-11-09 15:54:25 +0300

    display on reserved missions on 'My Profile' page

--- src/components/missions/MyMissions.js
+++ src/components/missions/MyMissions.js
@@ -1,5 +1,20 @@
-const MyMissions = () => (
-  <div />
-);
+import { useSelector } from 'react-redux';
+
+const MyMissions = () => {
+  const missions = useSelector((state) => state.missions.data);
+  const reservedMissions = missions.filter((m) => m.reserved);
+  return (
+    <div>
+      <h2>My Missions</h2>
+      <ul>
+        {
+          reservedMissions.map((m) => (
+            <li key={m.mission_id}>{m.mission_name}</li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
 
 export default MyMissions;
