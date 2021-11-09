c368aef8b786f7d2e567edec4f5d05484e3608eb
Bishoy <bishoy.samwuel@gmail.com>
2021-11-09 10:26:22 +0200

    Add Rockets to the main app file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
+import Rockets from './components/Rockets';
 import Header from './Header';
 
 function App() {
@@ -8,7 +9,9 @@ function App() {
       <Switch>
         <Route path="/" exact>this is hompeage</Route>
         <Route path="/missions" exact>these are missions</Route>
-        <Route path="/rockets" exact>these are rockets</Route>
+        <Route path="/rockets" exact>
+          <Rockets />
+        </Route>
         <Route path="/profile" exact>this is the profile</Route>
       </Switch>
     </>
